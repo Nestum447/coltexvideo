5eef23b8fae222aba8956fd175c99f8dd88b968a
Nestum447 <ncarpio935@gmail.com>
2025-09-06 13:38:19 -0600

    Create preprocess.js

---
+++ src/utils/preprocess.js
@@ -0,0 +1,31 @@
+export function preprocessImage(file, maxWidth = 1000) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+
+    img.onload = () => {
+      const canvas = document.createElement("canvas");
+      const scale = Math.min(maxWidth / img.width, 1);
+
+      canvas.width = img.width * scale;
+      canvas.height = img.height * scale;
+
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+      // Convertir a blanco y negro (threshold simple)
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const data = imageData.data;
+
+      for (let i = 0; i < data.length; i += 4) {
+        const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        const value = avg > 128 ? 255 : 0;
+        data[i] = data[i + 1] = data[i + 2] = value;
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+
+      canvas.toBlob((blob) => resolve(blob), "image/png");
+    };
+  });
+}
